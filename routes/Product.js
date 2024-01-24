8c2cce2246b84c3f611164d8c268de6179ec3849
Hemant2335 <knrt73373@gmail.com>
2024-01-24 22:01:39 +0530

    Added_the_products

--- routes/Product.js
+++ routes/Product.js
@@ -90,6 +90,16 @@ router.get("/all/Category" , async(req ,res)=>{
   }
 })
 
+router.get("/all/Category/:searchparam" , async(req , res) => {
+  try {
+    const catname = req.params.searchparam;
+    const cat = await Category.find({Name : catname});
+    res.status(200).json({Check: true , msg:"Category fetched Succesfully" , data : cat});
+  } catch (error) {
+    res.status(200).send({Check: false , msg:"Internal Server Error"})
+  }
+})
+
 
 
 
