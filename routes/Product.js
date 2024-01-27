b92ac9a20bb11d583c415d676011d80d5ce123c6
Hemant2335 <knrt73373@gmail.com>
2024-01-27 18:15:36 +0530

    check2

--- routes/Product.js
+++ routes/Product.js
@@ -92,9 +92,9 @@ router.get("/all/Category" , async(req ,res)=>{
 
 router.get("/all/filter" , async(req , res) => {
   try {
-    const catname = req.params.category;
-    const Subcatname = req.params.subcategory;
-    const type = req.params.type;
+    const catname = req.query.category;
+    const Subcatname = req.query.subcategory;
+    const type = req.query.type;
     const query = {};
     if (catname) {
       query.Category = catname;
